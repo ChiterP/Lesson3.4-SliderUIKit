36ac922bcb490e3525d02e6ad9178a90eb278a0f
ChiterP <63542970+ChiterP@users.noreply.github.com>
2021-06-11 11:27:52 +0300

    Initial Commit

---
+++ UI+KIT/UI_KITApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_KITApp.swift
+//  UI+KIT
+//
+//  Created by Алексей Сергейцев on 6/11/21.
+//
+
+import SwiftUI
+
+@main
+struct UI_KITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
